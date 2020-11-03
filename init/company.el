4b612d7b37c262137eeccaea6beb52c2c28b4420
squarespirit <matthew.wu@rubrik.com>
2020-11-03 12:46:38 -0600

    Load company on demand

--- init/company.el
+++ init/company.el
@@ -1,5 +1,11 @@
 (use-package company
-  :hook (after-init-hook . global-company-mode)
+  ;; TODO how to lazily load?
+  ;; (after-init-hook . global-company-mode) did not appear to work
+  ;; Needs to be demand so that it loads even before the first time
+  ;; I hit C-spc
+  :demand
+  :config
+  (global-company-mode 1)
   ;; TODO: What to bind completion function
   :bind (("C-SPC" . company-complete)))
 
