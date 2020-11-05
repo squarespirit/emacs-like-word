605f7a6e89e15b7db7846d6ea78f91cd3faaeb6f
squarespirit <matthew.wu@rubrik.com>
2020-11-05 13:41:26 -0600

    SP kill region or line

--- init/emacs-lisp.el
+++ init/emacs-lisp.el
@@ -3,9 +3,19 @@
 (defun mw-emacs-lisp-coding-defaults ()
   (smartparens-strict-mode +1))
 (add-hook 'emacs-lisp-mode-hook 'mw-emacs-lisp-coding-defaults)
+
+;; https://emacs.stackexchange.com/questions/28543/smartparens-strict-mode-c-w-kill-line-if-no-active-region
+(defun my-sp-kill-region-or-line (&optional arg)
+  "Kill active region or current line."
+  (interactive "p")
+  (if (use-region-p)
+      (sp-kill-region (region-beginning) (region-end)) ;; strict-mode version of kill-region
+    (sp-kill-whole-line))) ;; strict-mode version of kill-whole-line
+
 ;; Easier to reach
 (bind-keys
  :map emacs-lisp-mode-map
+ ("C-w" . my-sp-kill-region-or-line)
  ("<M-up>" . sp-up-sexp)
  ("<M-down>" . sp-down-sexp)
  ("<M-left>" . sp-backward-sexp)
