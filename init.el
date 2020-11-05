a117bce6625deac89a785cfd9fcef17cca9d6c3b
squarespirit <matthew.wu@rubrik.com>
2020-11-05 14:30:49 -0600

    Load files from init in alphabetical order

--- init.el
+++ init.el
@@ -1,60 +1,14 @@
 (defvar mw-init-dir (concat user-emacs-directory "init/")
   "Directory that contains all init files.")
 
-(dolist
-    (file '("gc.el"
+(mapc
+ 'load-file
+ ;; A few gotchas here:
+ ;; t for full paths.
+ ;; Exclude files starting with ., else . will be included
+ (directory-files mw-init-dir t "^[^#\.].*el$"))
 
-	    "packages.el"
-	    "bind-key.el"
-
-	    "esc.el"
-	    "whole-line-or-region.el"
-	    "kill-ring.el"
-	    "editing.el"
-	    "super-save.el"
-	    "ivy.el"
-	    "undo.el"
-	    "smartparens.el"
-	    "expand-region.el"
-	    "smex.el"
-	    "company.el"
-	    "imenu.el"
-	    "which-key.el"
-	    "multi-cursor.el"
-
-	    "magit.el"
-	    
-	    "window-management.el"
-
-	    "buffers.el"
-	    "buffers-tabs.el"
-	    "buffers-ctrl-tab.el"
-	    "buffers-by-name.el"
-
-	    "terminal.el"
-
-	    "looks.el"
-	    "scroll.el"
-	    "modeline.el"
-	    
-	    "files.el"
-	    "treemacs.el"
-	    
-	    "project.el"
-
-	    "dashboard.el"
-	    
-	    "session-config.el"
-
-	    "org-config.el"
-
-	    "emacs-lisp.el"))
-  (load-file (concat mw-init-dir file)))
-
-(setq custom-file (concat mw-init-dir "custom.el"))
-(if (file-exists-p custom-file)
-   (load-file custom-file))
-
-
-;; Load literate startup file
-;; (org-babel-load-file "~/.emacs.d/config.org")
+;; Custom file is already loaded above
+;; (setq custom-file (concat mw-init-dir "custom.el"))
+;; (if (file-exists-p custom-file)
+   ;; (load-file custom-file))
