0556b2372886df9aa7fd022548e14f4727699862
squarespirit <matthew.wu@rubrik.com>
2020-11-04 19:10:11 -0600

    Easier bindings for elisp

--- init/emacs-lisp.el
+++ init/emacs-lisp.el
@@ -3,3 +3,15 @@
 (defun mw-emacs-lisp-coding-defaults ()
   (smartparens-strict-mode +1))
 (add-hook 'emacs-lisp-mode-hook 'mw-emacs-lisp-coding-defaults)
+;; Easier to reach
+(bind-keys
+ :map emacs-lisp-mode-map
+ ("<M-up>" . up-list)
+ ("<M-down>" . down-list)
+ ("<M-left>" . backward-sexp)
+ ("<M-right>" . forward-sexp)
+ ("M-[" . insert-parentheses)
+ ("M-]" . move-past-close-and-reindent))
+
+;; Make this available everywhere
+(bind-keys ("C-M-x" . eval-defun))
