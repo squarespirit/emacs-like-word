1e54ac941c5925b8eef35fb356f271814c9b3ab9
Matthew Wu <mewu@caltech.edu>
2020-11-05 20:27:05 -0600

    Improved emacs lisp mode (copied from no-cua branch)

--- init/emacs-lisp.el
+++ init/emacs-lisp.el
@@ -3,13 +3,21 @@
 (defun mw-emacs-lisp-coding-defaults ()
   (smartparens-strict-mode +1))
 (add-hook 'emacs-lisp-mode-hook 'mw-emacs-lisp-coding-defaults)
+
 ;; Easier to reach
 (bind-keys
  :map emacs-lisp-mode-map
- ("<M-up>" . up-list)
- ("<M-down>" . down-list)
- ("<M-left>" . backward-sexp)
- ("<M-right>" . forward-sexp)
+ ("C-y" . sp-kill-whole-line)
+ ("<M-up>" . sp-up-sexp)
+ ("<M-down>" . sp-down-sexp)
+ ("<M-left>" . sp-backward-sexp)
+ ("<M-right>" . sp-forward-sexp)
+ ("<M-S-right>" . sp-forward-slurp-sexp)
+ ("<M-S-left>" . sp-forward-barf-sexp)
+ ("<M-backspace>" . sp-backward-kill-sexp)
+ ("<M-delete>" . sp-kill-sexp)
+ ("C-SPC" . sp-mark-sexp)
+ ("C-t" . sp-transpose-sexp)
  ("M-[" . insert-parentheses)
  ("M-]" . move-past-close-and-reindent))
 
